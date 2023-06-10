ac78f6a9d913d6a73b7cc094209fd7946e05f5ba
Roman Deriga <deriga.roman@gmail.com>
2023-06-10 20:09:33 +0700

    Edited session.py

--- db/session.py
+++ db/session.py
@@ -12,32 +12,3 @@ def get_db() -> Generator:
         yield connection
     finally:
         connection.close()
-        
-
-def create_db(db):
-
-    create_query = '''
-    create table employees(
-    id integer primary key autoincrement,
-    login text unique,
-    salary real,
-    increment_date date,
-    password_hash text
-    )'''
-
-    insert_query = '''
-    insert into employees 
-    (name, salary, increment_date, password_hash) values
-    ('Roman', 123.33, 08.06, '$2b$12$jYxDraYpMVasqX.zEkmxGO175YNTJyI8W.s1VhkazW6zGDw41en1a')
-    '''
-
-    #engine = create_engine(DB_PATH, echo=True)  
-    #with engine.connect() as conn:
-    db.execute(text(create_query))
-    db.execute(text(insert_query))
-        #conn.execute(text('insert into tablo values (1)'))
-    db.commit()        
-
-if __name__== '__main__':
-    db_conn = Depends(get_db)
-    create_db(db_conn)        
